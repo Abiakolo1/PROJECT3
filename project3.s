ed32d7aee5f193dd38108d4c01ac98e8e128a01e
ABiakolo1 <aghogho.biakolo@bison.howard.edu>
2018-12-12 20:42:41 +0200

    Making some of comments shorter and easier to read and planning what will go where

--- project3.s
+++ project3.s
@@ -9,19 +9,19 @@ main:
     li $v0, 8       #takes user input
     la $a0, userStr
     syscall
-    la $s5, userStr      #  the address of the string
+    la $s5, userStr #the address of the string
     li $t5, 0       #initialized i= 0
-    li $t1, 32      #  space here
+    li $t1, 32      #space here
     li $s0, 0       #initialized previous character to 0
     li $s7, 0       #initialized numofchracters
-    li $t6, 0x0A    #  new line here
+    li $t6, 0x0A    #new line here
     li $s6, 0       #num Spaces
     loop:
         lb $t7, 0($s5) #got a character of the string
         beq $t7, $t6, breakloop #break when newline
         beq $t7, $t1, noCharError #if the character is not a space and
         bne $s0, $t1, noCharError #if the previous character is a space and
-        beq $s7, $0, noCharError          #if the num of previously seen characters is not zero and
+        beq $s7, $zero, noCharError          #if the num of previously seen characters is not zero and
         beq $t7, $0, noCharError          #if the chLaracter is not null and
         beq $t7, $t6, noCharError         #if the character is not new line then print invalid
         li $v0, 4
